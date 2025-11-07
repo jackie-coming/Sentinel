6a788c5911793c0104b797c41b2b04ba63da9c7e
guojiaxiong <guojiaxiong399@hellobike.com>
2025-11-07 15:09:25 +0800

    update 1.8.6.map.3.SNAPSHOT

--- README.md
+++ README.md
@@ -63,7 +63,7 @@ If you're using Maven, just add the following dependency in `pom.xml`.
 <dependency>
     <groupId>com.alibaba.csp</groupId>
     <artifactId>sentinel-core</artifactId>
-    <version>1.8.6.map.2.RELEASE</version>
+  <version>1.8.6.map.3.SNAPSHOT</version>
 </dependency>
 ```
 
--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/Constants.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/Constants.java
@@ -31,7 +31,7 @@ import com.alibaba.csp.sentinel.util.VersionUtil;
  */
 public final class Constants {
 
-  public static final String SENTINEL_VERSION = VersionUtil.getVersion("1.8.6.map.2.RELEASE");
+  public static final String SENTINEL_VERSION = VersionUtil.getVersion("1.8.6.map.3.SNAPSHOT");
 
     public final static int MAX_CONTEXT_NAME_SIZE = 2000;
     public final static int MAX_SLOT_CHAIN_SIZE = 6000;
